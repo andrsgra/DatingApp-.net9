5e6ffb341e453db8247e87fdbf4da9e1b39b2fa0
andrsgra <andresgorostiaga95@gmail.com>
2025-01-08 18:14:25 -0300

    End of section 11

---
+++ API/Interfaces/IPhotoservice.cs
@@ -0,0 +1,10 @@
+using System;
+using CloudinaryDotNet.Actions;
+
+namespace API.Interfaces;
+
+public interface IPhotoservice
+{
+    Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
+    Task<DeletionResult> DeletePhotoAsync(string publicId);
+    }
